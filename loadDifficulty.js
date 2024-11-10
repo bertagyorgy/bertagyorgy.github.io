3ac08c6e924bb84c7bd93ec5a970ed8e59e54a85
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2024-11-10 14:33:00 +0100

    Delete loadDifficulty.js

--- loadDifficulty.js
+++
@@ -1,8 +0,0 @@
-// URL paraméter beolvasása
-const params = new URLSearchParams(window.location.search);
-const difficulty = params.get("difficulty") || "easy";
-
-// A megfelelő JavaScript fájl betöltése a nehézségi szint alapján
-let script = document.createElement("script");
-script.src = `botgame-${difficulty}.js`; // Például "botgame-easy.js"
-document.body.appendChild(script);
